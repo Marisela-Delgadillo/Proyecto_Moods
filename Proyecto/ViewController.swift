7fedfc6153e91d61641c203c478ba1361f36c677
Fer <reikafer@gmail.com>
2022-09-09 16:11:41 -0700

    proyecto terminado

--- Proyecto/ViewController.swift
+++ Proyecto/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var reproductorMoods : AVAudioPlayer?
+    var reproductorFondo : AVAudioPlayer?
+    
+    var url_happy : URL?
+    var url_sad : URL?
+    var url_angry : URL?
+    var url_inlove : URL?
+    
 
     @IBOutlet weak var lblMood: UILabel!
     @IBOutlet weak var imgHappy: UIImageView!
@@ -27,6 +37,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Audios
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        //Audio Fondo
+        do {
+            //Encontrar urñ deñ archivo de audio
+            let url = Bundle.main.url(forResource: "musica_fondo", withExtension: "mp3")
+            //Asignar valor al reproductor
+            reproductorFondo = try AVAudioPlayer(contentsOf: url!, fileTypeHint:  AVFileType.mp4.rawValue)
+            //reproducir
+            reproductorFondo?.volume = 0.2
+            reproductorFondo?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        //OBTENER URL DEL AUDIO
+        url_happy = Bundle.main.url(forResource: "Happy", withExtension : "m4a")
+        url_sad = Bundle.main.url(forResource: "Sad", withExtension : "m4a")
+        url_angry = Bundle.main.url(forResource: "Angry", withExtension : "m4a")
+        url_inlove = Bundle.main.url(forResource: "Inlove", withExtension : "m4a")
+        
         for i in 1...5 {
             let image = UIImage(named: "Flor2_feliz\(i)")
             secuenciaHappy.append(image!)
@@ -80,6 +117,13 @@ class ViewController: UIViewController {
     @IBAction func doTapHappy(_ sender: Any) {
         lblMood.text="Happy"
         
+        do {
+            reproductorMoods = try AVAudioPlayer(contentsOf: url_happy!, fileTypeHint: AVFileType.m4a.rawValue)
+            reproductorMoods?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        
         secuenciaMood  = []
         for i in 1...5 {
             let imagen = UIImage(named: "Flor_feliz\(i)")
@@ -93,6 +137,13 @@ class ViewController: UIViewController {
     @IBAction func doTapSad(_ sender: Any) {
         lblMood.text="Sad"
         
+        do {
+            reproductorMoods = try AVAudioPlayer(contentsOf: url_sad!, fileTypeHint: AVFileType.m4a.rawValue)
+            reproductorMoods?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        
         secuenciaMood  = []
         for i in 1...5 {
             let imagen = UIImage(named: "Flor_triste\(i)")
@@ -105,6 +156,12 @@ class ViewController: UIViewController {
     }
     @IBAction func doTapAngry(_ sender: Any) {
         lblMood.text="Angry"
+        do {
+            reproductorMoods = try AVAudioPlayer(contentsOf: url_angry!, fileTypeHint: AVFileType.m4a.rawValue)
+            reproductorMoods?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
         
         secuenciaMood  = []
         for i in 1...5 {
@@ -118,6 +175,34 @@ class ViewController: UIViewController {
     }
     @IBAction func doTapInLove(_ sender: Any) {
         lblMood.text="In love"
+        
+        do {
+            reproductorMoods = try AVAudioPlayer(contentsOf: url_inlove!, fileTypeHint: AVFileType.m4a.rawValue)
+            reproductorMoods?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
+        
+        secuenciaMood  = []
+        for i in 1...5 {
+            let imagen = UIImage(named: "Flor_enamorada\(i)")
+            secuenciaMood.append(imagen!)
+        }
+        imgMood.stopAnimating()
+        imgMood.animationImages = secuenciaMood
+        imgMood.animationDuration = 1.0
+        imgMood.startAnimating()
+    }
+    
+    @IBAction func doTapInLove2(_ sender: Any) {
+        lblMood.text="In love"
+        
+        do {
+            reproductorMoods = try AVAudioPlayer(contentsOf: url_inlove!, fileTypeHint: AVFileType.m4a.rawValue)
+            reproductorMoods?.play()
+        }catch let error {
+            print(error.localizedDescription)
+        }
         
         secuenciaMood  = []
         for i in 1...5 {
